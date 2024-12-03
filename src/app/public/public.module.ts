3d7421a2f5427294eb61fd6995f0c79a582a3d3b
Escoz <kyrovilches@gmail.com>
2024-12-03 13:14:25 +0100

    Nuevo módulo para los conjuros

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,14 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 import {PublicRoutingModule} from './public-routing.module';
-import {SpellFiltersComponent} from './components/spells/filters/spell-filters.component';
+import {SpellFiltersComponent} from './spells/components/filters/spell-filters.component';
 import {SpellsPageComponent} from './pages/spells-page/spells-page.component';
 import {PublicLayoutPageComponent} from './pages/layout-page/public-layout-page.component';
 import {SpellLevelPipe} from './pipes/spell-level.pipe';
-import {SpellCardComponent} from './components/spells/card/spell-card.component';
+import {SpellCardComponent} from './spells/components/card/spell-card.component';
 import {PrimeNgModule} from '../prime-ng/prime-ng.module';
 import {SpellImagePipe} from './pipes/spell-image.pipe';
-import {SpellDialogComponent} from './components/spells/dialog/spell-dialog.component';
+import {SpellDialogComponent} from './spells/components/dialog/spell-dialog.component';
 
 
 @NgModule({
---
+++ src/app/public/spells/spells.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {SpellsRoutingModule} from './spells-routing.module';
+
+
+@NgModule({
+	declarations: [],
+	imports: [
+		CommonModule,
+		SpellsRoutingModule
+	]
+})
+export class SpellsModule {
+}
