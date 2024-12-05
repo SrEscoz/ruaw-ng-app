69939b215d31a2d97395c76d7623d4c7209eb3d1
Escoz <kyrovilches@gmail.com>
2024-12-05 19:33:11 +0100

    Añadida eliminación de clases

--- src/app/admin/pages/admin-classes/admin-class-page/admin-class-page.component.ts
+++ src/app/admin/pages/admin-classes/admin-class-page/admin-class-page.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {SpellAdminService} from '../../../services/spell-admin.service';
 import {Router} from '@angular/router';
 import {ConfirmationService, MessageService} from 'primeng/api';
 import {ClassesService} from '../../../../public/services/classes.service';
 import {MinimalClass} from '../../../../public/interfaces/classes.interface';
+import {ClassAdminService} from '../../../services/class-admin.service';
 
 @Component({
 	selector: 'app-admin-class-page',
@@ -16,7 +16,7 @@ export class AdminClassPageComponent implements OnInit {
 
 	constructor(
 		private classService: ClassesService,
-		private spellAdminService: SpellAdminService,
+		private classAdminService: ClassAdminService,
 		private router: Router,
 		private messageService: MessageService,
 		private confirmationService: ConfirmationService
@@ -36,6 +36,50 @@ export class AdminClassPageComponent implements OnInit {
 	}
 
 	onDeleteClass(id: number, name: string): void {
+		this.confirmationService.confirm({
+			message: `¿Estás seguro de que quieres borrar ${name}?`,
+			header: 'Confirmar borrado',
+			icon: 'pi pi-exclamation-triangle',
+			acceptLabel: 'Si',
+			rejectLabel: 'No',
+			acceptIcon: 'none',
+			rejectIcon: 'none',
+			acceptButtonStyleClass: 'p-button-danger p-button-text',
+			rejectButtonStyleClass: 'p-button-secondary p-button-text',
+			defaultFocus: 'reject',
 
+			accept: () => {
+				this.classAdminService.deleteClass(id)
+					.subscribe(response => {
+						if (response) {
+							this.showSuccessToast(`${name} ha sido elimiado`);
+							this.loadClasses();
+						} else {
+							this.showErrorToast(`No ha sido posible eliminar ${name}`);
+						}
+					});
+			},
+			reject: () => {
+				return;
+			}
+		});
+	}
+
+	showSuccessToast(message: string): void {
+		this.messageService.add({
+			severity: 'success',
+			summary: 'Éxito',
+			detail: message,
+			life: 3000
+		});
+	}
+
+	showErrorToast(message: string): void {
+		this.messageService.add({
+			severity: 'error',
+			summary: 'Error',
+			detail: message,
+			life: 3000
+		});
 	}
 }
---
+++ src/app/admin/services/class-admin.service.ts
@@ -0,0 +1,51 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {BasicResponse, Spell} from '../../public/interfaces/spells.interface';
+import {catchError, map, Observable, of, throwError} from 'rxjs';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ClassAdminService {
+
+	private baseUrl = environment.baseUrl;
+
+	constructor(private http: HttpClient) {
+	}
+
+	saveSpell(spell: Spell): Observable<Spell | null> {
+		return this.http.post<Spell>(`${this.baseUrl}/spells`, spell)
+			.pipe(
+				catchError((err: HttpErrorResponse) => {
+					if (err.status === 409) {
+						return of(null);
+					}
+
+					return throwError(err);
+				})
+			);
+	}
+
+	updateSpell(spell: Spell): Observable<Spell | null> {
+		return this.http.put<Spell>(`${this.baseUrl}/spells/${spell.id}`, spell)
+			.pipe(
+				catchError((err: HttpErrorResponse) => {
+					if (err.status === 409) {
+						return of(null);
+					}
+
+					return throwError(err);
+				})
+			);
+	}
+
+	deleteClass(id: number): Observable<boolean> {
+		return this.http.delete<BasicResponse>(`${this.baseUrl}/classes/${id}`)
+			.pipe(
+				map(() => true),
+				catchError(() => of(false))
+			);
+	}
+
+}
