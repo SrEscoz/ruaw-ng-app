e7f8721a5ba498eee7e15d854becefe626d6263e
Escoz <kyrovilches@gmail.com>
2024-12-11 12:49:59 +0100

    Añadidas rutas y páginas básicas de login y registro

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,13 +2,17 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {AuthRoutingModule} from './auth-routing.module';
-import { AuthLayoutPageComponent } from './pages/layout-page/auth-layout-page.component';
+import {AuthLayoutPageComponent} from './pages/layout-page/auth-layout-page.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
+import {RegisterPageComponent} from './pages/register-page/register-page.component';
 
 
 @NgModule({
 	declarations: [
-    AuthLayoutPageComponent
-  ],
+		AuthLayoutPageComponent,
+		LoginPageComponent,
+		RegisterPageComponent
+	],
 	imports: [
 		CommonModule,
 		AuthRoutingModule
---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'auth-signin-page',
+	templateUrl: './register-page.component.html',
+	styles: ``
+})
+export class RegisterPageComponent {
+
+}
