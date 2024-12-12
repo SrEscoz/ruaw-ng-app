6da4b6c622b49888513aa41194536b87d27ed6bd
Escoz <kyrovilches@gmail.com>
2024-12-12 17:03:44 +0100

    Añadido botón para ir al panel de administración

--- src/app/public/components/public-banner/public-banner.component.ts
+++ src/app/public/components/public-banner/public-banner.component.ts
@@ -1,6 +1,7 @@
-import {Component, computed, inject} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../../../auth/services/auth.service';
+import {AuthStatus} from '../../../auth/interfaces/auth-status.enum';
 
 @Component({
 	selector: 'public-banner',
@@ -11,13 +12,15 @@ export class PublicBannerComponent {
 
 	private authService = inject(AuthService);
 	protected router = inject(Router);
-
-	public user = computed(() => this.authService.currentUser);
-	public authStatus = computed(() => this.authService.authStatus);
+	protected readonly AuthStatus = AuthStatus;
 
 	constructor() { }
 
 	isActiveRoute(route: string): boolean {
 		return this.router.isActive(route, false);
+	}
+
+	get authStatus(): AuthStatus {
+		return this.authService.authStatus();
 	}
 }
